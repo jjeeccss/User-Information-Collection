48ca691e1c33f06fb4ee46445423a2b348da5b9b
jjeeccss <jeicogacusana24@gmail.com>
2024-11-04 22:33:44 +0800

    initial commit

---
+++ userInformation.js
@@ -0,0 +1,9 @@
+let name = "Bella Gacusana";
+let age = 3;
+let favnum = 27;
+let favcolor = "brown";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favorate Number : " + favnum);
+console.log("Favorate Color : " + favcolor);
